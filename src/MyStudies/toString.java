91dd3d25292daf420e44c33d1837a306bf939db1
Herve-ndzye <hervendzye@gmail.com>
2025-10-10 09:14:58 +0200

    More clarification on the function of to strin in Exceptions

--- src/MyStudies/toString.java
+++ src/MyStudies/toString.java
@@ -6,6 +6,7 @@ public class toString {
             System.out.println("The division of 12 and  0 is ");
             System.out.println(12/0);
         }catch(Exception e){
+            System.out.println("This is the demonstration of working of to String function in exceptions");
             System.out.println(e.toString());
         }
     }
