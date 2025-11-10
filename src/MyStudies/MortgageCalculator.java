8e8ed6c795e3945c029373d69d002de4945a1eea
Herve-ndzye <hervendzye@gmail.com>
2025-11-10 19:02:34 +0200

    Refinement of mortgage calculator.

--- src/MyStudies/MortgageCalculator.java
+++ src/MyStudies/MortgageCalculator.java
@@ -8,45 +8,37 @@ public class MortgageCalculator {
     double interest;
     int period;
 
+    public MortgageCalculator(){}
+
     public MortgageCalculator(double principal, double interest, int period) {
         this.principal = principal;
         this.interest = interest/100/12;
         this.period = period*12;
     }
+    final Scanner c = new Scanner(System.in);
 
+    public double readInput(String prompt,int min,int max){
+        double value;
+        while(true){
+            System.out.print(prompt);
+            value = c.nextDouble();
+            if(value>=min &&  value<=max)
+                return value;
+            System.out.println(" Enter a number between "+min+" and "+max);
+        }
+    }
     double getMortgage(){
         return (principal*((interest*(Math.pow(1+interest,period))/(Math.pow(1+interest,period)-1))));
     }
 }
 class MortgageCalculatorMain{
     public static void main(String[] args){
-        Scanner c = new Scanner(System.in);
-        int t;
-        double p,r;
-        while(true){
-            System.out.print("Principal ($1k - $1M) : ");
-             p = c.nextDouble();
-            if(p>=1000 &&  p<=1000000)
-                break;
-            System.out.println("Enter a number between 1,000 and 1,000,000");
-        }
-            while(true){
-                System.out.print("Annual Interest Rate : ");
-                 r = c.nextDouble();
-                if(r>0 && r<=30)
-                    break;
-                System.out.println("Enter a number between 1 and 30");
-            }
-            while(true){
-                System.out.print("Period(Years) : ");
-                 t = c.nextInt();
-                if(t>0 && t<=30)
-                    break;
-                System.out.println("Enter a number between 1 and 30");
-            }
-            MortgageCalculator calc = new MortgageCalculator(p,r,t);
-            String result = NumberFormat.getCurrencyInstance().format(calc.getMortgage());
-            System.out.println("Mortgage : "+result);
-
+        MortgageCalculator calc = new MortgageCalculator();
+        double p = calc.readInput(" Principal : ",1000,1000000);
+        double r = calc.readInput(" Annual Interest Rate : ",1,30);
+        int t = (int)calc.readInput(" Period(Years) : ",1,30);
+        MortgageCalculator calculateMortgage = new MortgageCalculator(p,r,t);
+        String result = NumberFormat.getCurrencyInstance().format(calculateMortgage.getMortgage());
+        System.out.println(" Mortgage : "+result);
     }
 }
